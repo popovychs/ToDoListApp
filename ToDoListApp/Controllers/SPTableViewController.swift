eaaa6b8d84d1afa44ffbe1f88387284624b6f602
Stanislav Popovych <popovychs@gmail.com>
2016-04-28 19:59:36 +0300

    first init

---
+++ ToDoListApp/Controllers/SPTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SPTableViewController.swift
+//  ToDoListApp
+//
+//  Created by popovychs on 28.04.16.
+//  Copyright © 2016 popovychs. All rights reserved.
+//
+
+import UIKit
+
+class SPTableViewController: UITableViewController {
+
+}
